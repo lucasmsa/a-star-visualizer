c71acb0feabe4bc4cac58e9b1b0ce716885e91d6
Lucas <lucasmsea@outlook.com>
2021-09-08 13:38:59 -0300

    feat: save image w/ initial coordinates

--- src/utils/set_start_and_end_points.py
+++ src/utils/set_start_and_end_points.py
@@ -5,11 +5,11 @@ def mouse_click(event, x, y, flags, param):
     is_black_space = grid[y][x].any() == 0
     if event == cv2.EVENT_LBUTTONDOWN and not is_black_space:
         if globals()["start_point"] is None:
-            cv2.circle(grid, (x, y), 1, (42, 25, 84), 1)
+            cv2.circle(grid, (x, y), 1, (42, 25, 84), -1)
             globals()["start_point"] = (x, y)
             
         elif globals()["end_point"] is None:
-            cv2.circle(grid, (x, y), 1, (69, 23, 81), 1)
+            cv2.circle(grid, (x, y), 1, (69, 23, 81), -1)
             globals()["end_point"] = (x, y)
             
         refresh_image()
@@ -30,6 +30,7 @@ def handle_mouse_events(output_image_path: str):
         k = cv2.waitKey(20) & 0xFF
         if k == 27 or start_point and end_point:
             refresh_image()
+            cv2.imwrite(f"{output_image_path[:-4]}_with_initial_points.png", grid)
             time.sleep(1.5)
             cv2.destroyAllWindows()
             return start_point, end_point
